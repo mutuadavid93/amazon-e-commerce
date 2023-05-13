a702e03c3f1a32c5cb328d5ca48d2faf2256325e
David Mutua <mutuadavi@gmail.com>
2023-05-13 10:52:13 +0300

    ft: create main layout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,152 +1,132 @@
 <script setup>
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import { ref } from "vue";
+import { Link } from "@inertiajs/vue3";
+import MapMarkerOutlineIcon from "vue-material-design-icons/MapMarkerOutline.vue";
+import MenuDownIcon from "vue-material-design-icons/MenuDown.vue";
+import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+import CartMinusIcon from "vue-material-design-icons/CartMinus.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
-                                </Link>
-                            </div>
+  <div class="min-w-[1150px] bg-gray-100 h-full">
+    <div
+      class="flex items-center bg-gray-900 h-[60px] py-2 fixed z-50 min-w-[1150px] w-full"
+    >
+      <div class="flex">
+        <!-- NOTE: Link component is from inertia -->
+        <Link
+          class="text-white h-50px p-2 pt-3 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer"
+        >
+          AMAZON
+        </Link>
+      </div>
 
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
+      <div
+        class="text-white h-50px p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer"
+      >
+        <div class="flex items-center justify-center">
+          <MapMarkerOutlineIcon class="pt-2 -ml-1" fillColor="#F5F5F5" />
 
-                        <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {{ $page.props.auth.user.name }}
+          <div>
+            <div class="text-[13px] text-gray-300 font-extrabold">
+              <div>Delivery to John Doe</div>
+            </div>
 
-                                                <svg
-                                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-mr-2 flex items-center sm:hidden">
-                            <button
-                                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div
-                    :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
-                    class="sm:hidden"
-                >
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">
-                                {{ $page.props.auth.user.name }}
-                            </div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
+            <div class="text-[15px] text-white-300 -mt-1.5 font-extrabold">
+              <div>London SW2 SW2</div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <!-- grow translates to flex-grow: 1; allows the item to grow and take up any remaining space in the 
+flex container after all other flex items have taken up their specified widths -->
+      <div class="flex grow items-center h-[45px] px-1">
+        <div
+          class="flex items-center justify-center bg-gray-100 border-r border-r-gray-300 font-extrabold text-[11px] text-gray-600 w-[60px] h-[40px] rounded-l-md cursor-pointer"
+        >
+          <div class="pt-[3px]">All</div>
+
+          <!-- TIP:  -mr-2: move the icon 8px to the right.
+
+          focus:ring-0 removes the blue outline when the input is focused. 
+          i.e. it's like the input is not focused 
+           -->
+          <MenuDownIcon fillColor="#5E5E5E" :size="20" class="-mr-2" />
+        </div>
+
+        <input
+          type="text"
+          class="block w-full border-none border-transparent focus:border-transparent focus:ring-0"
+        />
+
+        <!-- NOTE:  p-[5px] on below div adds h-[40px] to make h-[45px] -->
+        <div class="cursor-pointer bg-orange-300 p-[5px] px-2 rounded-r-md">
+          <MagnifyIcon :size="30" />
+        </div>
+      </div>
+
+      <div class="flex">
+        <div
+          class="h-50px p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer"
+        >
+          <!-- justify-center: aligns the flex items along the horizontal axis at the center of the container. -->
+          <div class="flex items-center justify-center mt-2.5 px-1">
+            <img src="https://via.placeholder.com/35x20" alt="" />
+
+            <!-- -mt-2 -mr-0.5: move the text upword to align with prev element and push it closer to the right -->
+            <div class="text-[15px] text-white -mt-2 -mr-0.5 font-extrabold">EN</div>
+            <MenuDownIcon fillColor="#C2C2C2" :size="20" class="-mt-1.5 pr-1 -mr-4" />
+          </div>
+        </div>
+
+        <div
+          class="h-50px p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer"
+        >
+          <div class="flex items-center justify-center">
+            <div>
+              <div class="text-[12px] text-white font-extrabold">
+                Hello, <span>sign in</span>
+              </div>
+              <div class="flex items-center">
+                <div class="text-[15px] text-white -mt-1.5 font-extrabold">
+                  Account & List
+                </div>
+                <MenuDownIcon fillColor="#C2C2C2" :size="20" class="-mt-1 pr-1 -mr-4" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="h-50px p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer"
+        >
+          <div class="flex items-center justify-center">
+            <div>
+              <div class="text-[12px] text-white font-extrabold">Returns</div>
+              <div class="flex items-center">
+                <div class="text-[15px] text-white -mt-1.5 font-extrabold">& Orders</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="relative h-50px p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer"
+        >
+          <!-- NOTE: div can also have span parent -->
+          <span
+            class="absolute text-center right-[21px] w-[14px] top-0 rounded-full text-[20px]"
+          >
+            <div class="text-orange-400 font-extrabold bg-gray-900 h-[16px]">0</div>
+          </span>
+          <div class="flex items-center justify-center">
+            <CartMinusIcon fillColor="#FCFCFC" :size="40" class="-mt-0.5" />
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
