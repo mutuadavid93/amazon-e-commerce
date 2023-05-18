0916f4a3007deed5b7d18052bd44eb28c5b848cc
David Mutua <mutuadavi@gmail.com>
2023-05-18 16:42:59 +0300

    ft: work on stripe checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Order;
 
 class CheckoutController extends Controller
 {
@@ -12,15 +13,30 @@ class CheckoutController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Checkout');
-    }
+        $stripe = new \Stripe\StripeClient(
+            env('STRIPE_SECRET')
+        );
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $order = Order::where('user_id', '=', auth()->user()->id)
+            ->where('payment_intent', 'null')
+            ->first();
+
+        if (is_null($order)) {
+            return redirect()->route('checkout_success.index');
+        }
+
+        // Construct a payment intent
+        $intent = $stripe->paymentIntents->create([
+            // NOTE: amount is in cents i.e. 2000 = $20 e.g. 20.00 The dot is ommitted
+            'amount' => (int) $order->total,
+            'currency' => 'usd',
+            'payment_method_types' => ['card'],
+        ]);
+
+        return Inertia::render('Checkout', [
+            'intent' => $intent,
+            'order' => $order,
+        ]);
     }
 
     /**
@@ -28,23 +44,28 @@ class CheckoutController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        // Store the order in the database
+        $res = Order::where('user_id', '=', auth()->user()->id)
+            ->where('payment_intent', 'null')
+            ->first();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+        // If the order exists, update it
+        if (!is_null($res)) {
+            $res->total = $request->total;
+            $res->total_decimal = $request->total_decimal;
+            $res->items = json_encode($request->items);
+            $res->save();
+        } else {
+            // Otherwise, create a new order
+            $order = new Order();
+            $order->user_id = auth()->user()->id;
+            $order->total = $request->total;
+            $order->total_decimal = $request->total_decimal;
+            $order->items = json_encode($request->items);
+            $order->save();
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return redirect()->route('checkout.index');
     }
 
     /**
@@ -52,14 +73,12 @@ class CheckoutController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
-    }
+        $order = Order::where('user_id', '=', auth()->user()->id)
+            ->where('payment_intent', 'null')
+            ->first();
+        $order->payment_intent = $request['payment_intent'];
+        $order->save();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect()->route('checkout_success.index');
     }
 }
--- resources/js/Pages/Checkout.vue
+++ resources/js/Pages/Checkout.vue
@@ -2,6 +2,7 @@
   <Head title="Checkout" />
 
   <AuthenticateLayout>
+  {{ intent }}
     <div class="p-4 mt-2 max-w-[1250px] mx-auto text-3xl font-extrabold">Checkout</div>
 
     <div class="flex pt-4 max-w-[1250px] mx-auto">
@@ -66,10 +67,9 @@
 </template>
 
 <script setup>
-import { computed, toRefs } from "vue";
-import { Head, Link } from "@inertiajs/vue3";
+import { computed, toRefs, onMounted, ref } from "vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 import AuthenticateLayout from "@/Layouts/AuthenticatedLayout.vue";
-import CartOffIcon from "vue-material-design-icons/CartOff.vue";
 
 import { useCartStore } from "@/store/cart";
 import { storeToRefs } from "pinia";
@@ -79,9 +79,11 @@ const cartStore = useCartStore();
 // NOTE: `cart` is state property from the store.
 const { cart } = storeToRefs(cartStore);
 
-const removeFromCart = (_id) => {
-  cartStore.removeProductFromCart(_id);
-};
+const props = defineProps({
+  intent: Object,
+  order: Object,
+});
+const { intent } = toRefs(props);
 
 // NOTE: since cart is a reactive property, wrapping it in computed property means
 // whenever cart changes, total will be updated.
@@ -92,10 +94,124 @@ const total = computed(() => {
   return sum > 0 ? sum.toFixed(2) : 0;
 });
 
-// const totalWithoutDot = () => {
-//   let num = String(total.value).split(".").join("");
-//   return Number(num);
-// };
+const totalWithoutDot = () => {
+  let num = String(total.value).split(".").join("");
+  return Number(num);
+};
+
+let stripe = null;
+let elements = null;
+let card = null;
+let form = null;
+let isProcessing = ref(false);
+const data = useForm({ payment_intent: null });
+
+onMounted(() => {
+  // HINT: Always use the public key on the frontend.
+  // i.e. see -
+  stripe = Stripe(
+    "pk_test_51N94o9A4mZ3T3Xysv5QABJ0IKYCfpiuCK7M1SolTPy3TllZrVEumt6ahKJ4L7RgpwAdwsbxP8ZXgT0wlpLILqY3T002f8FRJj4"
+  );
+
+  elements = stripe.elements();
+  let style = {
+    base: {
+      color: "#32325d",
+      fontFamily: "Arial, sans-serif",
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#32325d",
+      },
+    },
+    invalid: {
+      fontFamily: "Arial, sans-serif",
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  };
+
+  card = elements.create("card", { style: style });
+  // Stripe injects an iframe into the DOM
+  card.mount("#card-element");
+
+  card.on("change", function (event) {
+    // Disable the Pay button if there are no card details in the Element
+    document.querySelector("button").disabled = event.empty;
+    document.querySelector("#card-error").textContent = event.error
+      ? event.error.message
+      : "";
+  });
+
+  form = document.getElementById("payment-form");
+  form?.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Complete payment when the submit button is clicked
+    payWithCard(stripe, card, intent.value.client_secret);
+  });
+}); // End of onMounted
+
+// Calls stripe.confirmCardPayment
+// If the card requires authentication Stripe shows a pop-up modal to
+// prompt the user to enter authentication details without leaving your page.
+const payWithCard = (stripe, card, clientSecret) => {
+  loading(true);
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+      },
+    })
+    .then(function (result) {
+      if (result.error) {
+        // Show error to your customer
+        showError(result.error.message);
+      } else {
+        // The payment succeeded!
+        orderComplete(result.paymentIntent.id);
+      }
+    });
+};
+
+// Shows a success message when the payment is complete
+const orderComplete = (paymentIntentId) => {
+  data.payment_intent = paymentIntentId;
+  data.put("/checkout");
+};
+
+let showError = (errorMsgText) => {
+  loading(false);
+  let errorMsg = document.querySelector("#card-error");
+  errorMsg.textContent = errorMsgText;
+  setTimeout(function () {
+    errorMsg.textContent = "";
+  }, 4000);
+};
+
+let loading = function (isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("button").disabled = true;
+    isProcessing.value = true;
+  } else {
+    document.querySelector("button").disabled = false;
+    isProcessing.value = false;
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+/* see - https://stripe.com/docs/payments/card-element?lang=php */
+#card-element {
+  border-radius: 4px 4px 0 0;
+  padding: 12px;
+  border: 1px solid rgba(50, 50, 93, 0.1);
+  height: 44px;
+  width: 100%;
+  background: white;
+}
+
+#payment-request-button {
+  margin-bottom: 32px;
+}
+</style>
